6fac34a41e276674e0d27693c08c5c2035d58248
eglove <hello@ethang.email>
2020-09-26 12:34:15 -0500

    run idea as admin

--- idea.ps1
+++ idea.ps1
@@ -1,1 +1,1 @@
-Start-Process 'C:\Users\thora\AppData\Local\JetBrains\Toolbox\apps\IDEA-U\ch-0\202.7319.50\bin\idea64.exe' .
+Start-Process 'C:\Users\thora\AppData\Local\JetBrains\Toolbox\apps\IDEA-U\ch-0\202.7319.50\bin\idea64.exe' . -Verb RunAs

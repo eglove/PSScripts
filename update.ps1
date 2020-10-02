4d646dca3451acd4aa52c3f9c5c421d21f88cee8
eglove <hello@ethang.email>
2020-10-02 08:08:58 -0500

    accept installation

--- update.ps1
+++ update.ps1
@@ -1,8 +1,8 @@
 ﻿Write-Host 'Updating Windows...'
 Get-WindowsUpdate
-Install-WindowsUpdate
+Install-WindowsUpdate -AcceptAll
 Write-Host 'Updating Powershell Modules...'
-Update-Module
+Update-Module -AcceptLicense
 Write-Host 'Updating Global Yarn Packages...'
 yarn global upgrade
 Write-Host 'Updating Software...'

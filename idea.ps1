846f7fd98710700e7a14271b74a5414abd889a5c
eglove <hello@ethang.email>
2020-10-07 19:34:02 -0500

    idea update

--- idea.ps1
+++ idea.ps1
@@ -1,1 +1,1 @@
-Start-Process 'C:\Users\thora\AppData\Local\JetBrains\Toolbox\apps\IDEA-U\ch-0\202.7319.50\bin\idea64.exe' . -Verb RunAs
+Start-Process 'C:\Users\thora\AppData\Local\JetBrains\Toolbox\apps\IDEA-U\ch-0\202.7660.26\bin\idea64.exe' . -Verb RunAs

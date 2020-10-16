e7a469f1002ecb67b4164ddbb1b57a4c45bef478
eglove <hello@ethang.email>
2020-10-16 17:20:58 -0500

    Add 7zip, .7z is a common archive extension, not recognize by Expand-Arhive command

--- pcSetup.ps1
+++ pcSetup.ps1
@@ -6,7 +6,7 @@ $psModules = @('PSWindowsUpdate')
 
 $yarnGlobals = @('mrm')
 
-$chocoPackages = @('0ad', 'anki', 'autoclicker', 'balabolka', 'everything', 'f.lux', 'foxitreader',
+$chocoPackages = @('0ad', '7zip', 'anki', 'autoclicker', 'balabolka', 'everything', 'f.lux', 'foxitreader',
 'geforce-experience', 'geforce-game-ready-driver', 'gh', 'git', 'google-backup-and-sync', 'GoogleChrome',
 'imagemagick', 'jetbrainstoolbox', 'jbs', 'microsoft-teams', 'microsoft-windows-terminal', 'minecraft-launcher',
 'nodejs', 'notepadplusplus', 'obs-studio', 'ojdkbuild', 'picpick.portable', 'powershell-core', 'speccy', 'steam',

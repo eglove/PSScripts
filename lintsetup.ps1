a44e9913de78f0fe256cf38f28d37e42ddb9ff31
eglove <hello@ethang.email>
2020-10-17 17:25:44 -0500

    add graphql plugin to eslint

--- lintsetup.ps1
+++ lintsetup.ps1
@@ -1,25 +1,10 @@
-[String[]]$dependencies =
-'styled-components'
+$dependencies = $('styled-components')
 
-[String[]]$devDependencies =
-'babel-eslint',
-'eslint',
-'eslint-config-airbnb',
-'eslint-config-prettier',
-'eslint-config-wesbos',
-'eslint-plugin-html',
-'eslint-plugin-import',
-'eslint-plugin-jsx-a11y',
-'eslint-plugin-prettier',
-'eslint-plugin-react',
-'eslint-plugin-react-hooks',
-'prettier',
-'stylelint',
-'stylelint-a11y',
-'stylelint-config-standard',
-'stylelint-config-styled-components',
-'stylelint-order',
-'stylelint-processor-styled-components'
+$devDependencies = @('babel-eslint', 'eslint', 'eslint-config-airbnb', 'eslint-config-prettier',
+'eslint-config-wesbos', 'eslint-plugin-graphql', 'eslint-plugin-html', 'eslint-plugin-import',
+'eslint-plugin-jsx-a11y', 'eslint-plugin-prettier', 'eslint-plugin-react', 'eslint-plugin-react-hooks', 'prettier',
+'stylelint', 'stylelint-a11y', 'stylelint-config-standard', 'stylelint-config-styled-components',
+'stylelint-order', 'stylelint-processor-styled-components')
 
 yarn add $dependencies
 yarn add --dev $devDependencies

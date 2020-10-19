28b95601cb4eefa4578dc264b5d0227955109946
eglove <hello@ethang.email>
2020-10-19 10:22:59 -0500

    add order config

--- lintsetup.ps1
+++ lintsetup.ps1
@@ -3,8 +3,8 @@ $dependencies = $('styled-components')
 $devDependencies = @('babel-eslint', 'eslint', 'eslint-config-airbnb', 'eslint-config-prettier',
 'eslint-config-wesbos', 'eslint-plugin-graphql', 'eslint-plugin-html', 'eslint-plugin-import',
 'eslint-plugin-jsx-a11y', 'eslint-plugin-prettier', 'eslint-plugin-react', 'eslint-plugin-react-hooks', 'prettier',
-'stylelint', 'stylelint-a11y', 'stylelint-config-standard', 'stylelint-config-styled-components',
-'stylelint-order', 'stylelint-processor-styled-components')
+'stylelint', 'stylelint-a11y', 'stylelint-config-idiomatic-order', 'stylelint-config-standard',
+'stylelint-config-styled-components', 'stylelint-order', 'stylelint-processor-styled-components')
 
 yarn add $dependencies
 yarn add --dev $devDependencies

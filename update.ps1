1a02a598b4ade512ed9e07339f676f49742a7115
eglove <hello@ethang.email>
2020-11-11 17:23:54 -0600

    changes to allow running at startup

--- update.ps1
+++ update.ps1
@@ -1,6 +1,6 @@
 ﻿Write-Host 'Updating Windows...'
 Get-WindowsUpdate
-Install-WindowsUpdate -AcceptAll
+Install-WindowsUpdate -AutoReboot -AcceptAll
 Write-Host 'Updating Powershell Modules...'
 Update-Module -AcceptLicense
 Write-Host 'Updating Global Yarn Packages...'
@@ -10,3 +10,5 @@ choco upgrade all
 Write-Host 'Complete!'
 Start-Process 'C:\Program Files\Anki\anki.exe'
 Start-Process 'https://docs.google.com/spreadsheets/d/16K1DybdEvhb3ng0mtlp02sd3M9aukLXB9fzKE9oCXKk/edit#gid=0'
+# Prevent exit when auto running
+Read-Host -Prompt "Press Enter to exit"

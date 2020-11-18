43c4c41de75f9b192cbdbcdd44269c4dd07a7f8d
eglove <hello@ethang.email>
2020-11-18 10:41:03 -0600

    Reverse list order, add resumerabbit

--- morning.ps1
+++ morning.ps1
@@ -1,18 +1,19 @@
 ﻿# web developer, front end developer, javascript, react
 $links = @(
-    'https://docs.google.com/spreadsheets/d/16K1DybdEvhb3ng0mtlp02sd3M9aukLXB9fzKE9oCXKk/edit#gid=0',
-    'https://www.linkedin.com/jobs/search/?currentJobId=2213595016&f_CF=f_WRA&f_E=2&f_JT=F&f_LF=f_AL&f_TPR=r86400&geoId=103644278&keywords=web%20developer&location=United%20States',
-    'https://www.linkedin.com/jobs/search/?f_CF=f_WRA&f_E=2&f_JT=F&f_LF=f_AL&f_TPR=r86400&geoId=103644278&keywords=front%20end%20developer&location=United%20States',
-    'https://www.linkedin.com/jobs/search/?f_CF=f_WRA&f_E=2&f_JT=F&f_LF=f_AL&f_TPR=r86400&geoId=103644278&keywords=javascript&location=United%20States',
-    'https://www.linkedin.com/jobs/search/?f_CF=f_WRA&f_E=2&f_JT=F&f_LF=f_AL&f_TPR=r86400&geoId=103644278&keywords=react&location=United%20States',
-    'https://www.linkedin.com/jobs/search/?f_E=2&f_JT=F&f_LF=f_AL&f_TPR=r86400&geoId=104428936&keywords=web%20developer&location=St%20Louis%2C%20Missouri%2C%20United%20States',
-    'https://www.linkedin.com/jobs/search/?f_E=2&f_JT=F&f_LF=f_AL&f_TPR=r86400&geoId=104428936&keywords=front%20end%20developer&location=St%20Louis%2C%20Missouri%2C%20United%20States',
-    'https://www.linkedin.com/jobs/search/?f_E=2&f_JT=F&f_LF=f_AL&f_TPR=r86400&geoId=104428936&keywords=javascript&location=St%20Louis%2C%20Missouri%2C%20United%20States',
-    'https://www.linkedin.com/jobs/search/?f_E=2&f_JT=F&f_LF=f_AL&f_TPR=r86400&geoId=104428936&keywords=react&location=St%20Louis%2C%20Missouri%2C%20United%20States',
-    'https://remotive.io/?live_jobs%5Bquery%5D=web%20developer&live_jobs%5BsortBy%5D=live_jobs_sort_by_date',
-    'https://remotive.io/?live_jobs%5Bquery%5D=front%20end%20developer&live_jobs%5BsortBy%5D=live_jobs_sort_by_date',
-    'https://remotive.io/?live_jobs%5Bquery%5D=javascript&live_jobs%5BsortBy%5D=live_jobs_sort_by_date',
-    'https://remotive.io/?live_jobs%5Bquery%5D=react&live_jobs%5BsortBy%5D=live_jobs_sort_by_date'
+'https://www.resumerabbit.com/mail',
+'https://remotive.io/?live_jobs%5Bquery%5D=react&live_jobs%5BsortBy%5D=live_jobs_sort_by_date',
+'https://remotive.io/?live_jobs%5Bquery%5D=javascript&live_jobs%5BsortBy%5D=live_jobs_sort_by_date',
+'https://remotive.io/?live_jobs%5Bquery%5D=front%20end%20developer&live_jobs%5BsortBy%5D=live_jobs_sort_by_date',
+'https://remotive.io/?live_jobs%5Bquery%5D=web%20developer&live_jobs%5BsortBy%5D=live_jobs_sort_by_date',
+'https://www.linkedin.com/jobs/search/?f_E=2&f_JT=F&f_LF=f_AL&f_TPR=r86400&geoId=104428936&keywords=react&location=St%20Louis%2C%20Missouri%2C%20United%20States',
+'https://www.linkedin.com/jobs/search/?f_E=2&f_JT=F&f_LF=f_AL&f_TPR=r86400&geoId=104428936&keywords=javascript&location=St%20Louis%2C%20Missouri%2C%20United%20States',
+'https://www.linkedin.com/jobs/search/?f_E=2&f_JT=F&f_LF=f_AL&f_TPR=r86400&geoId=104428936&keywords=front%20end%20developer&location=St%20Louis%2C%20Missouri%2C%20United%20States',
+'https://www.linkedin.com/jobs/search/?f_E=2&f_JT=F&f_LF=f_AL&f_TPR=r86400&geoId=104428936&keywords=web%20developer&location=St%20Louis%2C%20Missouri%2C%20United%20States',
+'https://www.linkedin.com/jobs/search/?f_CF=f_WRA&f_E=2&f_JT=F&f_LF=f_AL&f_TPR=r86400&geoId=103644278&keywords=react&location=United%20States',
+'https://www.linkedin.com/jobs/search/?f_CF=f_WRA&f_E=2&f_JT=F&f_LF=f_AL&f_TPR=r86400&geoId=103644278&keywords=javascript&location=United%20States',
+'https://www.linkedin.com/jobs/search/?f_CF=f_WRA&f_E=2&f_JT=F&f_LF=f_AL&f_TPR=r86400&geoId=103644278&keywords=front%20end%20developer&location=United%20States',
+'https://www.linkedin.com/jobs/search/?currentJobId=2213595016&f_CF=f_WRA&f_E=2&f_JT=F&f_LF=f_AL&f_TPR=r86400&geoId=103644278&keywords=web%20developer&location=United%20States',
+'https://docs.google.com/spreadsheets/d/16K1DybdEvhb3ng0mtlp02sd3M9aukLXB9fzKE9oCXKk/edit#gid=0'
 )
 
 function update {

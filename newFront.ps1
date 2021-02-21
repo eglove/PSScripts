38218c9ecff5534a3e45da4f4296cf3a8d357d6b
Ethan Glover <hello@ethang.email>
2021-02-21 14:23:45 -0600

    New project update w/ next: 	deleted:    lintsetup.ps1 	new file:   newFront.ps1 	renamed:    .eslintrc -> newProject/.eslintrc 	renamed:    .stylelintrc -> newProject/.stylelintrc 	new file:   newProject/GlobalStyles.css.js 	new file:   newProject/fonts/Raleway.ttf 	new file:   newProject/fonts/RalewayItalic.ttf

---
+++ newFront.ps1
@@ -0,0 +1,31 @@
+$dependencies = $( 'styled-components', 'styled-reset' )
+
+$devDependencies = @('babel-eslint', 'eslint', 'eslint-config-airbnb', 'eslint-config-prettier',
+'eslint-config-wesbos', 'eslint-plugin-graphql', 'eslint-plugin-html', 'eslint-plugin-import',
+'eslint-plugin-jsx-a11y', 'eslint-plugin-prettier', 'eslint-plugin-react', 'eslint-plugin-react-hooks', 'prettier',
+'stylelint', 'stylelint-a11y', 'stylelint-config-idiomatic-order', 'stylelint-config-prettier',
+'stylelint-config-standard', 'stylelint-config-styled-components', 'stylelint-order',
+'stylelint-processor-styled-components')
+
+if ($args[0] -ne $null)
+{
+    yarn create next-app $args[0]
+
+    cd $args[0]
+    yarn add $dependencies
+    yarn add --dev $devDependencies
+
+    Copy-Item $PSScriptRoot'/newProject/.stylelintrc' .stylelintrc
+    Copy-Item $PSScriptRoot'/newProject/.eslintrc' .eslintrc
+    Copy-Item $PSScriptRoot'/newProject/GlobalStyles.css.js' './styles/GlobalStyles.css.js'
+    Copy-Item -Recurse $PSScriptRoot'/newProject/fonts' './styles/fonts'
+
+    mrm lint-staged
+    mrm gitignore
+    mrm jest
+    mrm license --config:name 'Ethan Glover' --config:email 'hello@ethang.email'
+    Remove-Item 4
+    Remove-Item README.md
+} else {
+    Write-Host "Requires argument for name of project.";
+}

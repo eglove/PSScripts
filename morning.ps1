049be697b3516edb4d42511bf595e92bfbb738ad
Ethan Glover <hello@ethang.email>
2021-03-20 14:27:55 -0500

    auto update yarn packages

--- morning.ps1
+++ morning.ps1
@@ -45,6 +45,18 @@ function update
     (Get-Content .\installedPSModules.txt) | Where-Object { $_.trim() -ne "" } | Set-Content .\installedPSModules.txt
 
     Write-Output 'A' | yarn global upgrade
+    $installedYarnPackagesFile = '.\installedYarnPackages.txt'
+    $installedPackages = yarn global list
+    Out-File $installedYarnPackagesFile
+    ForEach ($line in $($installedPackages -split "`r`n"))
+    {
+        if ($line -match 'info')
+        {
+            $line | Select-String -Pattern "\b(?!info|has|binaries|\d|\d\d\b)[a-zA-Z-]+" -AllMatches | ForEach-Object {
+                Add-Content $installedYarnPackagesFile $_.matches.value.toString()
+            }
+        }
+    }
 
     choco upgrade all -y --skip-virus-check
     choco list -l -r --id-only | Out-File .\installedChocoPackages.txt

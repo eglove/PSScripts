42ca7b7f3c8b6a385b4634437bd16dcc9665c688
Ethan Glover <hello@ethang.email>
2021-06-18 20:05:19 -0500

    uncouple cdocker commands

--- cDocker.ps1
+++ cDocker.ps1
@@ -1,2 +1,6 @@
 # Stop Docker containers and remove images
-docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q) -f && docker rmi $(docker images -a -q) -f && docker system prune -a -f
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q) -f
+docker volume rm $(docker volume ls -q)
+docker rmi $(docker images -a -q) -f
+docker system prune -a -f
